4b2871c5580d3f6181f7217d026604adf2718fa6
Prince Tiwari <princetiwari.profes@gmail.com>
2024-08-30 16:50:58 +0530

    Intro of JavaScript

---
+++ Basics/1_intro.js
@@ -0,0 +1,12 @@
+// JavaScript was invented by Brendan Eich in 1995, and became an ECMA standard in 1997.
+// ECMA-262 is the official name of the standard. ECMAScript is the official name of the language.
+
+// JavaScript is a lightweight, cross-platform, single-threaded, and interpreted compiled programming language.
+
+// JavaScript can be used for Client-side developments as well as Server-side developments. 
+
+// How to Link JavaScript File in HTML ?
+
+// JavaScript can be added to HTML file in two ways:
+// Internal JS: We can add JavaScript directly to our HTML file by writing the code inside the <script> tag. The <script> tag can either be placed inside the <head> or the <body> tag according to the requirement.
+// External JS: We can write JavaScript code in another files having an extension.js and then link this file inside the <head> tag of the HTML file in which we want to add this code.
