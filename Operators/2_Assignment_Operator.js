d153dbc1ce825b590829118251719857060640bd
Prince Tiwari <princetiwari.profes@gmail.com>
2024-09-03 22:04:38 +0530

    Assignment Operator

---
+++ Operators/2_Assignment_Operator.js
@@ -0,0 +1,44 @@
+// The assignment operators in JavaScript are used to assign values to the variables.
+// These are binary operators.
+
+// Following are the list of assignment operator are: 
+/*
+    Assignment Operator	            Example	    Equivalent To
+= (Assignment)	                    a = b	    a = b
++= (Addition Assignment)	        a += b	    a = a + b
+-= (Subtraction Assignment)	        a -= b	    a = a – b
+*= (Multiplication Assignment)	    a *= b	    a = a * b
+/= (Division Assignment)	        a /= b	    a = a / b
+%= (Remainder Assignment)	        a %= b	    a = a % b
+**= (Exponentiation Assignment)	    a **= b	    a = a ** b   */
+
+
+/* ====== JavaScript Bitwise Assignment operators ====== */
+/* A bitwise assignment operator performs bitwise operation on the operands and assign the result to a variable.
+
+    Assignment Operator	            Example	    Equivalent To
+    &= (Bitwise AND Assignment)	    a &= b	    a = a & b
+    |= (Bitwise OR Assignment)	    a |= b	    a = a | b
+    ^= (Bitwise XOR Assignment)	    a ^= b	    a = a ^ b       */
+
+
+/*
+   ================== JavaScript Shift Assignment Operators =========================
+A shift assignment operator performs bitwise shift operation on the operands and assign the result to a variable (left operand).
+
+    Assignment Operator	                    Example	    Equivalent To
+    <<= (Left Shift Assignment)	            a <<= b	    a = a << b
+    >>= (Right Shift Assignment)	        a >>= b	    a = a >> b
+    >>>= (Unsigned Right Shift Assignment)	a >>>= b	a = a >>> b
+ */
+
+
+/*
+================ JavaScript Logical Assignment operators =================
+In JavaScript, a logical assignment operator performs a logical operation on the operands and assign the result to a variable (left operand).
+
+    Assignment Operator	                Example	    Equivalent To
+    &&= (Logical AND Assignment)	    a &&= b	    a = a && b
+    ||= (Logical OR Assignment)	        a ||= b	    a = a || b
+    ??= (Nullish Coalescing Assignment)	a ??= b	    a = a ?? b
+ */
