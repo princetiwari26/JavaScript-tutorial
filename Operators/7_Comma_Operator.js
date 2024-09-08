fc85291d198d28fc6b3ca9f0693981e9df317107
Prince Tiwari <princetiwari.profes@gmail.com>
2024-09-08 22:14:36 +0530

    Comma Operator

---
+++ Operators/7_Comma_Operator.js
@@ -0,0 +1,30 @@
+// Comma Operator mainly evaluates its operands from left to right sequentially and returns the value of the rightmost operand.
+
+// Syntax -> Expression1, Expression2, Expression3, ...so on
+
+// Example 1
+function Func1() {
+    console.log('one');
+    return 'one';
+}
+function Func2() {
+    console.log('two');
+    return 'two';
+}
+function Func3() {
+    console.log('three');
+    return 'three';
+}
+
+// Three expressions are
+// given at one place
+let x = (Func1(), Func2(), Func3());
+
+console.log(x);
+
+
+// Example 2
+console.log('Example 2 ---- ');
+for (let a = 0, b =5; a <= 5; a++, b--) {
+    console.log(a, b);
+}
