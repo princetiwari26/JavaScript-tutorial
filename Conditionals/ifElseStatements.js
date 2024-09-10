eef044fd864448171af2475a8cc429bc79c582cf
Prince Tiwari <princetiwari.profes@gmail.com>
2024-09-10 18:19:03 +0530

    if else statements

---
+++ Conditionals/ifElseStatements.js
@@ -0,0 +1,24 @@
+/* The JavaScript if-else statement is used to execute the code whether condition is true or false. There are three forms of if statement in JavaScript.
+
+If Statement
+If else statement
+if else if statement
+
+*/
+
+/*
+JavaScript if statement
+The if statement is the fundamental control statement that allows JavaScript to make decisions and execute statements conditionally.
+
+Syntax −
+if (expression) {
+   Statement(s) to be executed if expression is true
+}
+*/
+let result;
+      let age = 20;
+      if( age > 18 ) {
+         result = "Qualifies for driving";
+      }
+      console.log(result);
+      
