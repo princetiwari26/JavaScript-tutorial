5fc40838e94201eff2670074778beeb8eef32702
Prince Tiwari <princetiwari.profes@gmail.com>
2024-09-21 06:59:43 -0700

    For loop

---
+++ Loops/for loop/for_loop.js
@@ -0,0 +1,13 @@
+// iterates the elements for the fixed number of times.
+
+/*
+Syntax- 
+for (initialization; condition; increment)  
+{  
+    code to be executed  
+}  
+*/
+
+for (i = 1; i <= 5; i++) {
+    console.log(i , '\n')
+}  
